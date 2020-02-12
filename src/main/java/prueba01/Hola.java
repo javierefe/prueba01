c1489a0d844dbef0d8521bbd8227f15a37793c8e
javierefe <jcfloresq@gmail.com>
2020-02-12 14:04:55 -0500

    1er commit

---
+++ src/main/java/prueba01/Hola.java
@@ -0,0 +1,21 @@
+package prueba01;
+
+import java.util.Scanner;
+
+public class Hola {
+
+	public static void main(String[] args) {
+		//imprimir mensaje 
+		System.out.println("Bienvenidos al curso");
+		
+		//capturar valores de la consolsa
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("¿Cual es tu nombre?: ");
+		String nombre =sc.nextLine();
+		System.out.println("Bienvenido "+nombre);
+		
+		sc.close();
+	}
+
+}
